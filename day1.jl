f53b76d6fb8a20ceb8619e5e8d9d79a064dd3c02
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-12-10 17:10:11 +0100

    finish d1p2

--- day1.jl
+++ day1.jl
@@ -17,12 +17,26 @@ end
 
 # Part 2
 function part2(lines)
-    # Your solution here
-    return 0
+    cnt = 0
+    cur_dial = 50
+    for line in lines
+        # first character 'L' or 'R', the rest is number
+        dir = line[1] == 'L' ? -1 : 1
+        num = parse(Int, line[2:end])
+
+        cnt += div(num, 100)
+        num %= 100
+        next_dial = cur_dial + dir * num
+        if cur_dial != 0 && (next_dial >= 100 || next_dial <= 0)
+            cnt += 1
+        end
+        cur_dial = mod(next_dial, 100)
+    end
+    return cnt
 end
 
 # Read input
 lines = readlines("data/input_d01")  # or this for line-by-line
-println(part1(lines))
+println(part2(lines))
 
 
