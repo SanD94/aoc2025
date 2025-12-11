706f897f0dfab934a593e2cd6794fe2f91023c76
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-12-11 16:27:01 +0100

    add editorconfig for Julia

---
+++ day2.jl
@@ -0,0 +1,14 @@
+# Part 1
+function part1(lines)
+
+end
+
+# Part 2
+function part2(lines)
+end
+
+# Read input
+lines = readlines("data/input_d02")  # or this for line-by-line
+println(part2(lines))
+
+
