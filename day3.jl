da7f89c5a7d23226872130bc63767ca6c9e046a6
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-12-12 16:16:22 +0100

    solve d3p2

--- day3.jl
+++ day3.jl
@@ -12,8 +12,40 @@ end
 
 # Part 2
 function part2(lines)
+    total = 0
+    for line in lines
+        digits = line
+        b, e = 1, length(line)
+        selected_indices = Int[]
+        for digit in 9:-1:0
+            cur_char = Char('0' + digit)
+            cur_b, cur_e = b, e
+            for i in b:e
+                if length(selected_indices) == 12
+                    break
+                end
+                if cur_char == digits[i]
+                    push!(selected_indices, i)
+                    left = 12 - length(selected_indices)
+                    if i + left > e
+                        cur_e = i - 1
+                        append!(selected_indices, collect(i+1:e))
+                        break
+                    else
+                        cur_b = i + 1
+                    end
+                end
+            end
+            b, e = cur_b, cur_e
+        end
+        sort!(selected_indices)
+        selected_digits = digits[selected_indices]
+        num = parse(Int, string(selected_digits...))
+        total += num
+    end
+    return total
 end
 
 # Read input
 lines = readlines("data/input_d03")  # or this for line-by-line
-println(part1(lines))
+println(part2(lines))
