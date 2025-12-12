4bf2ca9ec936191d845ce044b98da93f29a4f7f4
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-12-12 17:00:11 +0100

    solve d4p2

--- day4.jl
+++ day4.jl
@@ -2,7 +2,7 @@
 function part1(lines)
     grid = [collect(line) for line in lines]
     rows, cols = length(grid), length(grid[1])
-    
+
     count = 0
     for r in 1:rows
         for c in 1:cols
@@ -25,8 +25,42 @@ end
 
 # Part 2
 function part2(lines)
+    grid = [collect(line) for line in lines]
+    rows, cols = length(grid), length(grid[1])
+
+    total_removed = 0
+    while true
+        to_remove = []
+        for r in 1:rows
+            for c in 1:cols
+                if grid[r][c] == '@'
+                    neighbors = 0
+                    for (dr, dc) in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+                        nr, nc = r + dr, c + dc
+                        if 1 <= nr <= rows && 1 <= nc <= cols && grid[nr][nc] == '@'
+                            neighbors += 1
+                        end
+                    end
+                    if neighbors < 4
+                        push!(to_remove, (r, c))
+                    end
+                end
+            end
+        end
+
+        if isempty(to_remove)
+            break
+        end
+
+        for (r, c) in to_remove
+            grid[r][c] = '.'
+        end
+        total_removed += length(to_remove)
+    end
+
+    return total_removed
 end
 
 # Read input
 lines = readlines("data/input_d04")  # or this for line-by-line
-println(part1(lines))
+println(part2(lines))
