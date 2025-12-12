dfcd480663b0628736cf9e1429b23c6e151622a2
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-12-12 17:02:59 +0100

    refactor d4

--- day4.jl
+++ day4.jl
@@ -1,52 +1,40 @@
-# Part 1
-function part1(lines)
-    grid = [collect(line) for line in lines]
-    rows, cols = length(grid), length(grid[1])
+const DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-    count = 0
+function find_accessible(grid)
+    rows, cols = length(grid), length(grid[1])
+    accessible = []
     for r in 1:rows
         for c in 1:cols
             if grid[r][c] == '@'
                 neighbors = 0
-                for (dr, dc) in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+                for (dr, dc) in DIRECTIONS
                     nr, nc = r + dr, c + dc
                     if 1 <= nr <= rows && 1 <= nc <= cols && grid[nr][nc] == '@'
                         neighbors += 1
                     end
                 end
                 if neighbors < 4
-                    count += 1
+                    push!(accessible, (r, c))
                 end
             end
         end
     end
-    return count
+    return accessible
+end
+
+# Part 1
+function part1(lines)
+    grid = [collect(line) for line in lines]
+    return length(find_accessible(grid))
 end
 
 # Part 2
 function part2(lines)
     grid = [collect(line) for line in lines]
-    rows, cols = length(grid), length(grid[1])
 
     total_removed = 0
     while true
-        to_remove = []
-        for r in 1:rows
-            for c in 1:cols
-                if grid[r][c] == '@'
-                    neighbors = 0
-                    for (dr, dc) in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-                        nr, nc = r + dr, c + dc
-                        if 1 <= nr <= rows && 1 <= nc <= cols && grid[nr][nc] == '@'
-                            neighbors += 1
-                        end
-                    end
-                    if neighbors < 4
-                        push!(to_remove, (r, c))
-                    end
-                end
-            end
-        end
+        to_remove = find_accessible(grid)
 
         if isempty(to_remove)
             break
